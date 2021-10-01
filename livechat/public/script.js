0d980fbbdf1cba15dbc8ae5729b9032a7747ece5
Ven Kim <venvex@gmail.com>
2021-10-01 11:47:55 -0400

    added ejs and updated server for livechat rooms

--- livechat/public/script.js
+++ livechat/public/script.js
@@ -3,9 +3,21 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
+if (messageForm != null) {
+  const name = prompt('What is your name?')
+  appendMessage('You joined')
+  socket.emit('new-user', name)
+
+  messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+  })
+}
+
+
 
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`)
@@ -19,13 +31,7 @@ socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected`)
 })
 
-messageForm.addEventListener('submit', e => {
-  e.preventDefault()
-  const message = messageInput.value
-  appendMessage(`You: ${message}`)
-  socket.emit('send-chat-message', message)
-  messageInput.value = ''
-})
+
 
 function appendMessage(message) {
   const messageElement = document.createElement('div')
--- livechat/server.js
+++ livechat/server.js
@@ -1,4 +1,33 @@
-const io = require('socket.io')(3000, {
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+const rooms = { name: {}}
+
+app.get('/', (req, res) => {
+  res.render('index', { room: rooms })
+})
+
+app.post('/room', (req, res) => {
+  if (rooms[req.body.room] != null) {
+    return res.redirect('/')
+  }
+  rooms[req.body.room] = { users: {} }
+  res.redirect(req.body.room)
+  // Send message that new room was created
+})
+
+app.get('/:room', (res, req) => {
+  res.render('room', { roomName: req.params.room })
+})
+
+server.listen(3000, {
   cors: {
     origin: "*",
   },
