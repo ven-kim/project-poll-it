4c311b4a5dd24383cf8fb6548c2ff9848016e335
Ven Kim <venvex@gmail.com>
2021-10-01 12:23:17 -0400

    restructured livechat rooms due to errors

--- livechat/server.js
+++ livechat/server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
-const rooms = { name: {}}
+const rooms = { }
 
 app.get('/', (req, res) => {
   res.render('index', { room: rooms })
@@ -21,9 +21,13 @@ app.post('/room', (req, res) => {
   rooms[req.body.room] = { users: {} }
   res.redirect(req.body.room)
   // Send message that new room was created
+  io.emit('room-created', req.body.room)
 })
 
 app.get('/:room', (res, req) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/')
+  }
   res.render('room', { roomName: req.params.room })
 })
 
@@ -33,18 +37,26 @@ server.listen(3000, {
   },
 });
 
-const users = {}
-
 io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).broadcast.emit('user-connected', name)
   })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).broadcast.emit('chat-message', { message: message, name: users[socket.id] })
   })
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
+    getUserRooms(socket).forEach(room => {
+      socket.broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    })
   })
 })
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
